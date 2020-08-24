c177fcc79106807876afc3ddf8eb1a123e2eece2
Martin Grotz <martin.grotz@gmx.de>
2020-08-24 15:02:19 +0200

    move file parsing into separate action

--- code/FSharpRefactor/InvoiceParser.fs
+++ code/FSharpRefactor/InvoiceParser.fs
@@ -6,6 +6,11 @@ open System.Linq
 
 
 module InvoiceParser =
+    type RawInvoice = {
+        contents: string list
+        errorText: string
+    }
+        
     type InvoiceSum = {
         sum: decimal
         discountedSum: decimal
@@ -22,43 +27,61 @@ module InvoiceParser =
         errors: string list
     }
     
+    let parseFile path =
+        try
+            let lines = System.IO.File.ReadLines(path) |> Seq.toList
+            {
+                contents = lines
+                errorText = null
+            }
+        with
+        | ex -> {
+            contents = []
+            errorText = ex.Message
+        }
+    
+    let parseFiles paths =
+        Seq.map parseFile paths
+        |> Seq.toList
+ 
     let parseInvoices (paths : IEnumerable<string>) (discountPercentage : Nullable<decimal>) (isDiscountAllowed : Nullable<bool>) : SumOrErrors =
         let parsedInvoices = List<ParseResult>()
         
-        for filePath in paths do
+        for rawInvoice in (parseFiles paths) do
             let parsedInvoice =
-                try
-                    let lines = System.IO.File.ReadLines(filePath)
-                
-                    if lines.Count() = 1 then
-                        let success, parsedAmount = Decimal.TryParse(lines.First())
-                        if success then
-                            let discounted =
-                                if discountPercentage.HasValue
-                                    && isDiscountAllowed.HasValue
-                                    && isDiscountAllowed.Value then
-                                        Nullable<decimal>(parsedAmount - (parsedAmount * (discountPercentage.Value / 100m)))
-                                else
-                                    Nullable<decimal>()
-                            
-                            {
-                                ParseResult.errorText = null 
-                                amount = Nullable<decimal>(parsedAmount)
-                                discountedAmount = discounted
-                            }
-                        else
-                            { ParseResult.errorText = "The file content could not be parsed"
-                              amount = Nullable<decimal>()
-                              discountedAmount = Nullable<decimal>() }
+                    if not (String.IsNullOrEmpty rawInvoice.errorText) then
+                         {
+                             ParseResult.errorText = rawInvoice.errorText
+                             amount = Nullable<decimal>()
+                             discountedAmount = Nullable<decimal>()
+                        }   
                     else
-                        {   ParseResult.errorText = "The file must have exactly one line"
-                            amount = Nullable<decimal>()
-                            discountedAmount = Nullable<decimal>() }
-                with
-                | ex -> {
-                                          ParseResult.errorText = ex.Message
-                                          amount = Nullable<decimal>()
-                                          discountedAmount = Nullable<decimal>() }
+                        let lines = rawInvoice.contents
+                        if lines.Length = 1 then
+                            let success, parsedAmount = Decimal.TryParse(lines.First())
+                            if success then
+                                let discounted =
+                                    if discountPercentage.HasValue
+                                        && isDiscountAllowed.HasValue
+                                        && isDiscountAllowed.Value then
+                                            Nullable<decimal>(parsedAmount - (parsedAmount * (discountPercentage.Value / 100m)))
+                                    else
+                                        Nullable<decimal>()
+                                
+                                {
+                                    ParseResult.errorText = null 
+                                    amount = Nullable<decimal>(parsedAmount)
+                                    discountedAmount = discounted
+                                }
+                            else
+                                { ParseResult.errorText = "The file content could not be parsed"
+                                  amount = Nullable<decimal>()
+                                  discountedAmount = Nullable<decimal>() }
+                        else
+                            {   ParseResult.errorText = "The file must have exactly one line"
+                                amount = Nullable<decimal>()
+                                discountedAmount = Nullable<decimal>() }
+               
             parsedInvoices.Add(parsedInvoice)
                 
         if parsedInvoices.Any(fun invoice -> not (String.IsNullOrEmpty(invoice.errorText))) then
